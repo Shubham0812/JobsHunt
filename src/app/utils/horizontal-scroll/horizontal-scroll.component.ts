f70887a3cf2609acf8060d978c1b3e81b6fcfdd7
Shubham Singh <shubham.singh12@infosys.com>
2019-09-28 19:01:45 +0530

    Changes in Horizontal Scroller

--- src/app/utils/horizontal-scroll/horizontal-scroll.component.ts
+++ src/app/utils/horizontal-scroll/horizontal-scroll.component.ts
@@ -65,12 +65,6 @@ export class HorizontalScrollComponent implements OnInit, OnDestroy {
   }
 
   updateNavigationButtonStatus(element: HTMLElement) {
-    console.log(
-      "scroll width",
-      element.scrollWidth,
-      " client width",
-      element.clientWidth
-    );
     if (element.scrollWidth > element.clientWidth) {
       this.disableNext = false;
     } else {
